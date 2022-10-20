660d125a36f2fc786bdbb6c1d0e6384b89be35fa
AssemMostafa <aasim@telgani.com>
2022-10-20 16:06:36 +0200

    Add Mock target

--- RobustaTask/ViewControllerProvider.swift
+++ RobustaTask/ViewControllerProvider.swift
@@ -7,20 +7,41 @@
 
 import Foundation
 
+enum AppConfiguration {
+    case live
+    case mock
+}
+#if MOCK
+let appConfiguration: AppConfiguration = .mock
+#else
+let appConfiguration: AppConfiguration = .live
+#endif
+
 struct ViewControllerProvider {
     private init() {}
 
-
     static var repositoriesViewController: RepositoriesViewController {
-        let repositoriesService = RepositoriesService()
-        let viewModel = RepositoriesViewModel(service: repositoriesService)
+        let repositoriesService: RepositoriesServicing
+        switch appConfiguration {
+        case .live:
+            repositoriesService = RepositoriesService()
+        case .mock:
+            repositoriesService = MockRepositoriesService()
+        }
         let vc = RepositoriesViewController()
+        let viewModel = RepositoriesViewModel(service: repositoriesService)
         vc.viewModel = viewModel
         return vc
     }
 
      static func navigateToRepoProfileVC(with repoOwner: Owner) ->  RepositoryProfileViewController {
-         let repositoryProfileService = RepositoryProfileService()
+         let repositoryProfileService: RepositoryProfileServicing
+         switch appConfiguration {
+         case .live:
+             repositoryProfileService = RepositoryProfileService()
+         case .mock:
+             repositoryProfileService = MockRepositoryProfileService()
+         }
         let vc = RepositoryProfileViewController()
         let repositoryProfileViewModel = RepositoryProfileViewModel(ownerRepo: repoOwner, service: repositoryProfileService)
         vc.viewModel = repositoryProfileViewModel
